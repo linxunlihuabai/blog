0132c795aa622d1a25ecd42f60d51ea0c881d78f
sunfangyuan <983235934@qq.com>
2021-07-29 23:02:01 +0800

    feat: 防抖函数

---
+++ javaScript/deboundce/deboundce.js
@@ -0,0 +1,43 @@
+function deboundce(fn,wait,immediate){
+    var timeout;
+    var result;
+    function deboundced(){
+        const context = this;
+        const args = arguments;
+        clearTimeout(timeout);
+        if(immediate && !timeout){
+            result = fn.apply(context,args);
+            timeout = setTimeout(() => {
+                fn.apply(context,args);
+                timeout = null;
+            }, wait);
+        }else{
+            timeout = setTimeout(() => {
+                fn.apply(context,args);
+            }, wait);
+        }
+        return result;
+    }
+
+    deboundced.onCancel = function() {
+        clearTimeout(timeout);
+        timeout = null;
+    }
+
+    return deboundced;
+}
+
+var container = document.getElementById('container');
+var cancel = document.getElementById('cancel');
+var count = 1;
+
+function setContainer(e) {
+    console.log(e, this);
+    container.innerText = count++;
+}
+
+var deboundceFn = deboundce(setContainer, 1000);
+
+
+document.onmousemove = deboundceFn;
+cancel.onclick = deboundceFn.onCancel;
